fb7177cf4fd6fb7b0c3d8a01613dda6ac53f7ad2
Tom Bauer <tbauer1898@gmail.com>
2020-10-19 22:22:53 -0500

    added start script

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 7500;
+const port = process.env.PORT;
 server.listen(port, () => {
     console.log(`\nServer is cooking at ${port}\n`)
 });
