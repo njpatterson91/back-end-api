7a1fecc1d994acf6d079b4863dee9a50abd3ad85
Tom Bauer <tbauer1898@gmail.com>
2020-10-21 12:10:33 -0500

    Ability to assign items added

--- potlucks/potluckModel.js
+++ potlucks/potluckModel.js
@@ -23,7 +23,7 @@ function findItemsByPotluckId(id) {
     return db("potlucks as p")
         .where("p.id", '=', id)
         .join("potluck_requirements as pr", "p.id", '=', 'pr.potluck_id')
-        .select("p.id", "pr.item_category", "pr.item_name", 'pr.item_description', 'pr.item_amount', 'pr.assigned_to_user_id')
+        .select("pr.id", "pr.potluck_id", "pr.item_category", "pr.item_name", 'pr.item_description', 'pr.item_amount', 'pr.assigned_to_user_id')
 }
 
 function findItemsByUserAndPotluckId(user_id, potluck_id) {
